4664e936efeda0cc21f468c3af69eb3510fbb898
BraianGomez9 <braiangomez_orni@hotmail.com>
2025-10-24 19:24:34 -0300

    menu hamburguesa en navbar desplegable y detalles responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,42 @@
-import React from 'react'
-import "../main.css"
-import { Link } from 'react-router-dom'
-import CartWidget from './CartWidget'
+import "../main.css";
+import { Link } from 'react-router-dom';
+import CartWidget from './CartWidget';
+import { useState } from "react";
+
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const closeMenu = () => {
+        setIsOpen(false);
+    };
+
     return (
         <nav className='navbar'>
-            <Link to="/"><h1>Toronja Shop</h1></Link>
-            <ul className='menu'>
-                <li><Link className="menu-link" to="/">Inicio</Link></li>
-                <li><Link className="menu-link" to="/productos">Productos</Link></li>
-                <li><Link className="menu-link" to="/productos/medias">Medias</Link></li>
-                <li><Link className="menu-link" to="/productos/pantalones">Pantalones</Link></li>
-                <li><Link className="menu-link" to="/productos/remeras">Remeras</Link></li>
-                <li><Link className="menu-link" to="/productos/buzos">Buzos</Link></li>
-                <li><Link className="menu-link" to="/nosotros">Nosotros</Link></li>
-                <li><Link className="menu-link" to="/contacto">Contacto</Link></li>
+            <Link to="/" className="logo"><h1>Toronja Shop</h1></Link>
+
+            {/* Botón hamburguesa */}
+            <button className="menu-toggle" onClick={toggleMenu}>
+                ☰
+            </button>
+
+            {/* Menú principal */}
+            <ul className={`menu ${isOpen ? "open" : ""}`}>
+                <li><Link className="menu-link" to="/" onClick={closeMenu}>Inicio</Link></li>
+                <li><Link className="menu-link" to="/productos" onClick={closeMenu}>Productos</Link></li>
+                <li><Link className="menu-link" to="/productos/medias" onClick={closeMenu}>Medias</Link></li>
+                <li><Link className="menu-link" to="/productos/pantalones" onClick={closeMenu}>Pantalones</Link></li>
+                <li><Link className="menu-link" to="/productos/remeras" onClick={closeMenu}>Remeras</Link></li>
+                <li><Link className="menu-link" to="/productos/buzos" onClick={closeMenu}>Buzos</Link></li>
+                <li><Link className="menu-link" to="/nosotros" onClick={closeMenu}>Nosotros</Link></li>
+                <li><Link className="menu-link" to="/contacto" onClick={closeMenu}>Contacto</Link></li>
                 <li><CartWidget /></li>
             </ul>
         </nav>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
